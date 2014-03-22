76032dd568e81e9b0e9355e062b63ab58504b09c
Eric <ehowey@gmail.com>
2014-03-22 10:24:34 -0600

    Added screen shot, removed m1-sidebar-two.php

--- m1-sidebar-two.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * M1 Sidebar two, included in the main files.  Second sidebar to display, defaults to
- in between sidebar one and content
- */
-
-// must be run from within DokuWiki
-if (!defined('DOKU_INC')) die();
-?>
